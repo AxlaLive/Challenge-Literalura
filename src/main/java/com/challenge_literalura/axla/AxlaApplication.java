ff553c57b62072ebf67b57b9e789095c6a1837b5
AxlaLive <alexagarav@hotmail.com>
2025-06-22 15:03:47 -0500

    Primer commit de prueba

---
+++ src/main/java/com/challenge_literalura/axla/AxlaApplication.java
@@ -0,0 +1,13 @@
+package com.challenge_literalura.axla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxlaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxlaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge_literalura/axla/AxlaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge_literalura.axla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxlaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
